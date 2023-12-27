6acf699e0b5d485f1d2c60cd71ba6b5d15702afc
Attilio Greco <attilio.greco@4sigma.it>
2023-12-27 11:05:41 +0100

    update build proces

---
+++ setup.py
@@ -0,0 +1,16 @@
+# from setuptools import setup, find_packages
+#
+# setup(
+#     name='django_tabbed_admin2',
+#     version_format='{tag}.dev{commitcount}+{gitsha}',
+#     setup_requires=['setuptools-git'],
+#     packages=find_packages(),
+#     install_requires=[],
+#     entry_points={},
+#     author='4Sigma',
+#     author_email='devops@4sigma.it',
+#     description='Tabbed Admin for Django',
+#     url='https://github.com/4Sigma/django-tabbed-admin',
+#     long_description=open('README.rst').read(),
+#     long_description_content_type='text/x-rst',
+# )
